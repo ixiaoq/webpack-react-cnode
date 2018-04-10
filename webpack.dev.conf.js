bbc2cd6210dff8d7ee7a46164946d0a3f8ef8c1a
yaoxiaoqiang <372632349@qq.com>
2018-04-10 17:25:47 +0800

    frist commit

--- webpack.dev.conf.js
+++ webpack.dev.conf.js
@@ -27,7 +27,7 @@ module.exports = {
     devServer: {
         contentBase: path.join(__dirname, './dist'),
         historyApiFallback: true,
-        host: '192.168.1.31',
+        host: 'localhost',
         port: 8080
     },
 
